0c12fdd92c92c0df40403563151dab991ef09ca9
mabuit03 <mabuit03@hs-esslingen.de>
2023-02-06 03:34:07 +0100

    alles

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ window.addEventListener("load", () => {
 });
 
 const smash = {
+  checkboxList: ["Yes", "Definitely", "Absolutely!!!!"],
   content: document.createElement("div"),
+  currentlSelected: -1,
   init() {
     this.content.id = "content";
     document.body.appendChild(this.content);
     this.generateHeader();
     this.generateMainText();
+    this.generateQuestion();
+    this.generateBtn();
   },
+
   generateHeader() {
     const header = document.createElement("div");
     header.classList.add("header");
     this.content.appendChild(header);
     const title = document.createElement("p");
     title.innerHTML = "❤️";
-    title.id = "HeartTitle";
+    title.id = "heartTitle";
     header.appendChild(title);
   },
 
@@ -28,24 +33,82 @@ const smash = {
     const title = document.createElement("h");
     title.classList.add("textTitle");
     textBody.appendChild(title);
-    loadFile("text.txt");
+    title.innerHTML = "Hello There!!";
 
     const text = document.createElement("p");
     text.id = "paragraphText";
     text.classList.add("textParagraph");
-    text.innerHTML = getText();
     textBody.appendChild(text);
+    setText();
+  },
+
+  generateQuestion() {
+    const sheet = document.createElement("div");
+    sheet.classList.add("sheet");
+    this.content.appendChild(sheet);
+    const question = document.createElement("p");
+    question.innerHTML = `Willst du mit <br/>&nbsp &nbsp mir gehen?`;
+    question.id = "question";
+    sheet.appendChild(question);
+    for (let i = 0; i < 3; i++) {
+      this.generateCheckbox(sheet, i);
+    }
+  },
+
+  generateCheckbox(parent, id) {
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("checkbox");
+    checkbox.type = "radio";
+    checkbox.name = "name";
+    checkbox.value = id;
+    checkbox.id = "checkbox" + id;
+    checkbox.addEventListener("clicked", (id) => this.selectOnlyThis(id));
+
+    let label = document.createElement("label");
+    label.htmlFor = "checkbox1";
+    label.classList.add("checkboxLable");
+    label.appendChild(document.createTextNode(this.checkboxList[id]));
+
+    let row = document.createElement("div");
+    row.classList.add("checkboxRow");
+    row.appendChild(checkbox);
+    row.appendChild(label);
+    parent.appendChild(row);
+  },
+
+  generateBtn() {
+    const btn = document.createElement("button");
+    btn.classList.add("submitBtn");
+    btn.innerHTML = "Bestätigen";
+    this.content.appendChild(btn);
+    btn.addEventListener("click", () => this.submitBtnClicked());
+  },
+  submitBtnClicked() {
+    let selectedBox = this.selectedBtn();
+    if (selectedBox != -1) {
+      Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "malte.budig@elasticemail.de",
+        Password: "E4969DC6E8FC7FD3B388A5BB7A693A85D300",
+        To: "simon.mueller2@outlook.de",
+        From: "mabu@vollbio.de",
+        Subject: "Was hat sie wohl geantwortet ...",
+        Body: "Die Antwort ist \n\n\n " + this.checkboxList[selectedBox],
+      });
+    } else {
+      alert("Pls select one UwU");
+    }
+  },
+  selectedBtn() {
+    let radios = document.querySelectorAll('input[type="radio"]:checked');
+    let value = radios.length > 0 ? radios[0].value : -1;
+    return value;
   },
 };
 
-function loadFile(o) {
-  var fr = new FileReader();
-  fr.onload = function (e) {
-    showDataFile(e, o);
-  };
-  fr.readAsText(o.files[0]);
-}
-
-function showDataFile(e, o) {
-  document.getElementById("paragraphText").innerText = e.target.result;
+async function setText() {
+  const response = await fetch("text.json");
+  const json = await response.json();
+  console.log(json.text);
+  document.getElementById("paragraphText").innerHTML = json.text;
 }
