244e27f02b1b2e7632289f2323da343fc3a12a1c
Doderich <malte.budig@gmx.net>
2023-02-08 22:17:19 +0100

    new port

--- start.js
+++ start.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(80, () => {
+app.listen(PORT, () => {
   console.log("Application started and Listening on port 3000");
 });
 
